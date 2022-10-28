3b2db63e6877f1b99741f99e6ec6678c6cb414ae
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-28 16:29:42 +0600

    show user current location when clicking the map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import LocationMarker from './Components/LocationMarker';
 
 function App() {
+  const position = [23.725380014793473, 90.41183175368344];
+
   return (
-    <MapContainer
-      center={[23.725380014793473, 90.41183175368344]}
-      zoom={10}
-      scrollWheelZoom={true}
-    >
+    <MapContainer center={position} zoom={10} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[23.725380014793473, 90.41183175368344]}>
+      {/* <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
-      </Marker>
+      </Marker> */}
+      <LocationMarker />
     </MapContainer>
   );
 }
